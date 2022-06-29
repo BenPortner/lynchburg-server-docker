b1604cef083db987701c199d1787aba2dbd7f219
Nils G <nils.gondermann@ruhr-uni-bochum.de>
2022-06-29 11:21:23 +0000

    Scheduler waits for database initialization

--- files/start_scheduler.sh
+++ files/start_scheduler.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -f $WEB2PY_ROOT/applications/app/databases/sql.log ]
+then
+        echo "Waiting for database initialization..."
+        while [ ! -f $WEB2PY_ROOT/applications/app/databases/sql.log ]
+        do
+                sleep 3
+        done
+        echo "Database initialized"
+fi
+
 until python3 $WEB2PY_ROOT/web2py.py -K app; do
         echo "Scheduler exited with $?" >&2
         sleep 3
